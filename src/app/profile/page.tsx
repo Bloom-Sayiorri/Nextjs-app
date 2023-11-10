dcbfba3dc6e44350e510ddef2754a6f2111431b3
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-10 19:12:52 +0300

    signup and login styling done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,9 @@
 export default function ProfilePage() {
     return(
-        <div className='flex justify-center'>
-            <h1>Profile</h1>
+        <div className='flex justify-center items-center flex-col min-h-screen'>
+            <h1 className=" text-white">Profile</h1>
             <hr/>
-            <p>Profile Page</p>
+            <p className=" text-white text-4xl">Profile Page</p>
         </div>
     )
 };
