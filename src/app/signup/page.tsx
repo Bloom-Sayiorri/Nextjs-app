cc9773d8c134181840ecd0976d88bc55d87a623b
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-10 17:28:09 +0300

    styling signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -17,7 +17,7 @@ export default function SignupPage() {
 
 
     return(
-        <div className="flex justify-center flex-col gap-4">
+        <div className='flex justify-center flex-col gap-4'>
             <h1 className='text-center text-white text-2xl '>Create Account</h1>
             <label htmlFor="username" className='text-white'>Username</label>
             <input
@@ -28,6 +28,26 @@ export default function SignupPage() {
                 className="rounded-lg border-radius h-10 w-1/4 p-4"
                 onChange={(e) => setUser({...user, username: e.target.value})}
             />
+            <label htmlFor='email' className='text-white'>Email</label>
+            <input
+                id='email'
+                type='email'
+                placeholder='Enter your email address'
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+            />
+            <label htmlFor='password' className='text-white'>Password</label>
+            <input
+                id='password'
+                type='password'
+                placeholder='Enter your password address'
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
+            />
+            <button className='border-none rounded-lg p-4 text-white bg-cyan-400' onSubmit={(e) => {
+                e.preventDefault();
+
+            }}>Submit</button>
         </div>
     )
 } 
