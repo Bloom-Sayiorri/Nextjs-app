4753de9296ddece0af617d3176a5dce30882256a
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-12 23:39:53 +0300

    signup route changes

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,2 +1,39 @@
-import {connect} from '@dbCongif/dbConfig.ts';
-import {axios} from 'axios';
+import {connect} from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import {NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+
+connect()
+
+export default async function POST (request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const {username, email, password} = reqBody
+
+        console.log(reqBody);
+
+        //check if user already exists
+        const user = await User.findOne({email})
+        if (user) {
+            return NextResponse.json({error: 'User already exists!!!'},
+            {status: 400});
+        }
+        else if (!user) {
+            return NextResponse.json({error: 'User not found!!!'},
+            {status: 404});
+        }
+
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
+        const newUser = new User ({
+
+        })
+
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}),
+        {status: 500}
+    }
+};
