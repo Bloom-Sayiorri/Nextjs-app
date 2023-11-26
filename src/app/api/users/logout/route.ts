1735c913868c638ce8a65f4f2596196244780c47
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-26 23:36:05 +0300

    logout page and route

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    try {
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},
+        {status: 500});
+    }
+}
