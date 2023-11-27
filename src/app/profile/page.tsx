b20f9932fbb88ccb32f7d2993c0531376575d52e
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-27 22:25:01 +0300

    profile page done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import axios from 'axios';
 import Link from 'next/link';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 export default function ProfilePage() {
 
-    const logout = () => {
+    const router = useRouter();
+
+    const logout = async () => {
         try {
-            
+            await axios.get('/api/users/logout')
+            toast.success('Logout Successful')
+            router.push('/login');
         } catch (error) {
             
         }
