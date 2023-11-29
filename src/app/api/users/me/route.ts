e4f731ba6a1bb0e8721004aff71c73db5117d10b
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-29 23:44:34 +0300

    user profile

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -7,9 +7,14 @@ connect();
 
 export async function GET(request: NextRequest) {
     try {
-        
-    } catch (error) {
-        
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select('-password');
+        return NextResponse.json({
+            message: 'User found',
+            data: user
+        })
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},
+        {status: 400});
     }
 }
-// getDataFromToken
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import Link from 'next/link';
 import { toast } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 export default function ProfilePage() {
 
     const router = useRouter();
+    const [data, setData] = useState('nothing');
 
     const logout = async () => {
         try {
@@ -18,18 +20,27 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me');
+        console.log(res.data);
+        setData(res.data.data._id);
+    };
+
     return(
         <div className='flex justify-center items-center flex-col min-h-screen py-2'>
             <h1 className=" text-white">Profile</h1>
             <hr/>
             <p className=" text-white text-4xl">Profile Page</p>
+            <h2 className='p-3 rounded bg-green-500'>{data === 'nothing' ? 'Nothing' : <Link href={`/profile/${data}`}></Link>}</h2>
             <hr/>
             <button
                 onClick={logout}
                 className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-blo\d py-2 px-4 rounded"
-            >
-                Logout
-            </button>
+            >Logout</button>
+            <button
+                onClick={getUserDetails}
+                className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-blo\d py-2 px-4 rounded"
+            >Get User Details</button>
         </div>
     )
 };
