6644bbd519f271ffa83ee841c617f4a46d1798de
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-07 23:47:03 +0300

    mailers file changes

--- src/helpers/mailers..ts
+++ src/helpers/mailers..ts
@@ -1,7 +1,15 @@
 import nodemailer from 'nodemailer';
-import Uder from '@/models/userModel';
+import User from '@/models/userModel';
 import bcryptjs from 'bcryptjs';
 
-export const sendEmail = async({email, password}) {
+export const sendEmail = async({email, emailType, userId}: any) {
+    try {
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
 
+        await User.findByIdAndUpdate(userId,
+            {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000}, {new: true, runValidators: true}
+        )
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
 }
