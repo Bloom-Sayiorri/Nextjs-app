c1d5ff6972db19cb3d72fc8786430066d371d4b4
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-07 23:59:01 +0300

    changes

--- src/helpers/mailers..ts
+++ src/helpers/mailers..ts
@@ -13,11 +13,11 @@ export const sendEmail = async({email, emailType, userId}: any) {
         } else if (emailType === 'RESET')
         await User.findByIdAndUpdate(userId,
             {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000}, {new: true, runValidators: true}
-            )
-        }
-
+        )
         
-    } catch (error: any) {
+        const 
+    } 
+    catch (error: any) {
         throw new Error(error.message);
     }
 }
