6cf8579a5be3f83275d545210b312d341cc8880c
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-07 23:55:26 +0300

    mailers changes

--- src/helpers/mailers..ts
+++ src/helpers/mailers..ts
@@ -6,9 +6,17 @@ export const sendEmail = async({email, emailType, userId}: any) {
     try {
         const hashedToken = await bcryptjs.hash(userId.toString(), 10)
 
-        await User.findByIdAndUpdate(userId,
+        if (emailType === 'VERIFY') {
+            await User.findByIdAndUpdate(userId,
             {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000}, {new: true, runValidators: true}
-        )
+            )
+        } else if (emailType === 'RESET')
+        await User.findByIdAndUpdate(userId,
+            {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000}, {new: true, runValidators: true}
+            )
+        }
+
+        
     } catch (error: any) {
         throw new Error(error.message);
     }
